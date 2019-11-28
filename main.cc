9c75cb149e2166ab885a0da81cefafc4194fb2ab
Matthew <matthewzmd@gmail.com>
2019-11-28 04:00:35 -0500

    Working cmdInterpreter

--- main.cc
+++ main.cc
@@ -1,8 +1,33 @@
 #include <iostream>
 #include "game.h"
 
-std::string cmdInterpreter(const std::string& cmd, const std::string cmdList[]);
 
+/* Tries to match command cmd with the list cmdList and returns the matched command string.
+   If cannot find the match string, or matched more than 1 command, return empty string
+ */
+std::string cmdInterpreter(const std::string& cmd, const std::vector<std::string>& cmdList, int i = 0){
+    if(cmd.size() == 0 && cmdList.size() > 1){
+        // Matched more than 1 command
+        return "";
+    }
+    std::vector<std::string> newList;
+    // Loop through the command
+    for(int l = 0; l < cmdList.size(); ++l){
+        if(cmdList.at(l).size() > i && cmd.at(0) == cmdList.at(l).at(i)){
+            newList.emplace_back(cmdList.at(l));
+        }
+    }
+    if(newList.size() == 0){
+        // Cannot find any match string
+        return "";
+    } else if (newList.size() == 1){
+        return newList.at(0);
+    } else {
+        return cmdInterpreter(cmd.substr(1), newList, i + 1);
+    }
+}
+
+/* Main Program */
 int main(int argc, char* argv[]) {
     // True for text-only display, false showing both text and graphics
     bool textOnly = false;
@@ -37,10 +62,12 @@ int main(int argc, char* argv[]) {
 
     std::shared_ptr<Game> g = std::make_shared<Game>(fileName1, fileName2, startLevel);
 
+    std::string cmd;
     bool isRunning = true;
     while(isRunning){
         try{
-
+            std::cin >> cmd;
+            cmd = cmdInterpreter(cmd, cmdList);
         } catch (const std::string s){
             // Game Over
             std::cout << s << std::endl;
@@ -50,29 +77,4 @@ int main(int argc, char* argv[]) {
     // Game finished
     std::cout << g->playerTurn << " has lost!" << std::endl;
 
-}
-
-/* Tries to match command cmd with the list cmdList and returns the matched command string.
-   If cannot find the match string, or matched more than 1 command, return empty string
- */
-std::string cmdInterpreter(const std::string& cmd, const std::vector<std::string>& cmdList){
-    if(cmd.size() == 0 && cmdList.size() > 1){
-        // Matched more than 1 command
-        return "";
-    }
-    std::vector<std::string> newList;
-    // Loop through the command
-    for(int l = 0; l < cmdList.size(); ++l){
-        if(cmd.at(0) == cmdList.at(l).at(0)){
-            newList.emplace_back(cmdList.at(l));
-        }
-    }
-    if(newList.size() == 0){
-        // Cannot find any match string
-        return "";
-    } else if (newList.size() == 1){
-        return newList.at(0);
-    } else {
-        return cmdInterpreter(cmd.substr(1), newList);
-    }
 }
