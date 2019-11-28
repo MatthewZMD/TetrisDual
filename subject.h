f1989cee3e2b47554a0c95bf143a478ee55a5c2f
Mingde Zeng <m8zeng@maltodextrin.csclub.uwaterloo.ca>
2019-11-28 01:17:20 -0500

    change shared_ptr into raw point of concreteboard

--- subject.h
+++ subject.h
@@ -11,9 +11,9 @@ class Cell;
 
 class Subject {
     public:
-    std::vector<std::shared_ptr<Observer>> observers;
+    std::vector<Observer*> observers;
     virtual std::vector<std::vector<Cell>> getTestInfo();
-    void attach(std::shared_ptr<Observer> o);
+    void attach(Observer* o);
     void notifyObservers();
     virtual Info& getInfo() = 0;
     virtual State& getState() = 0;
