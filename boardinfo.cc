5277feeff2717a9d2cb52f1d5cc5342a84d50a30
Matthew <matthewzmd@gmail.com>
2019-11-28 19:13:43 -0500

    Fix conflict

--- boardinfo.cc
+++ boardinfo.cc
@@ -1,10 +1,7 @@
 #include "boardinfo.h"
 
-<<<<<<< Updated upstream
-=======
 BoardInfo::BoardInfo() : player{0}, level{0}, score{0}, boardData{std::vector<std::vector<char>>{}}, nextType{CellType::E} {}
 
->>>>>>> Stashed changes
 BoardInfo::BoardInfo(int player, int level, int score, std::vector<std::vector<char>> boardData, CellType nextType) : player{player}, level{level}, score{score}, boardData{boardData}, nextType{nextType} {}
 
 BoardInfo::~BoardInfo() {}
