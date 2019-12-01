15fd662a8d8eeffda82737479ff9b2775a52b484
Matthew <matthewzmd@gmail.com>
2019-11-30 23:31:33 -0500

    Fix make

--- graphicsdisplay.h
+++ graphicsdisplay.h
@@ -1,20 +1,20 @@
 #ifndef _GRAPHICSDISPLAY_H_
 #define _GRAPHICSDISPLAY_H_
-
 #include "window.h"
 #include "board.h"
 #include "observer.h"
 #include "info.h"
 #include "state.h"
+#include "boardinfo.h"
 
 class GraphicsDisplay: public Observer {
 	private:
-		Xwindow xw;
-		int length;
-		int width;
+    int length;
+    int width;
+    Xwindow xw;
 	public:
-		void notify(Subject& s) override;
-		GraphicsDisplay(int length, int width);
-		~GraphicsDisplay();
+    void notify(Subject& s) override;
+    GraphicsDisplay(int length, int width);
+    ~GraphicsDisplay();
 };
 #endif
